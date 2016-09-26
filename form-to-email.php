793f5616a70d5217a650da00a3088e1c631bcd1f
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-09-26 00:07:25 -0400

    initial project version

---
+++ form-to-email.php
@@ -0,0 +1,18 @@
+<?php
+	$name = $_POST["name"];
+	$visitor_email = $_POST["email"];
+	$message = $_POST["message"];
+
+	$email_from = "dana.m.dietrich11@gmail.com";
+	$email_subject = "New Form submission";
+	$email_body = "You have received a new message from the user $name.\n".
+	    "Here is the message:\n $message".
+	    
+	$to = "dana.m.dietrich11@gmail.com";
+	$headers = "From: $email_from \r\n";
+	$headers .= "Reply-To: $visitor_email \r\n";
+
+	mail($to,$email_subject,$email_body,$headers);
+
+	header("Location: thank-you.html");
+?>
---
+++ script.js
@@ -0,0 +1,6 @@
+var string = "<input type="text" />"
+
+function makeMoreExciting(string) {
+	return string + '!!!'
+	console.log(string)
+}
