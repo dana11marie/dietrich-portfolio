b02ab739c35cf44afcf0ef299a2c9d996017e7e4
Dana Dietrich <dana.m.dietrich11@gmail.com>
2016-10-13 21:05:44 -0400

    added jquery to nav

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ function yell() {
 	var newText = originalText.toUpperCase();
 	document.getElementById('newYellText').innerText = newText;
   }
+
+var $nav = $("li.nav");
+$nav.css('width', '200px');
+$nav.css('height', '50px');
+
+$nav.hover(
+	function(){
+	$(this).fadeTo(100, 0.25);
+	$(this).css('text-transform', 'uppercase');
+},function(){
+	$(this).fadeTo(100, 1);
+	$(this).css('text-transform', 'lowercase');
+});
+
+//Example of taking an action on a click:
+// $nav.click(function(){
+// 		$(this).css('font-size', '40px');
+// });
